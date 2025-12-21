992365c2e3604fa535b5ec56ce0aea23e66e3997
Jerome <cheonjh4572@gmail.com>
2025-12-21 16:27:43 +0900

    Update w/ nested struct and basic struct receiver

--- main.go
+++ main.go
@@ -2,15 +2,36 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
-	alex := person{
-		firstName: "Alex",
-		lastName:  "Anderson",
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
 	}
-	fmt.Println(alex)
+	jim.updateName("jimmy")
+	jim.print()
+
+}
+
+// doesn't work due to it's not a pointer receiver
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
