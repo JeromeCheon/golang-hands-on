279c299af51fe6d8dfb78946ab7a77c77221375d
Jerome <cheonjh4572@gmail.com>
2026-01-04 16:16:47 +0900

    Update with channel and go routine to run infinitely

--- ch7/main.go
+++ ch7/main.go
@@ -14,16 +14,24 @@ func main() {
 		"https://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go checkLink(l, c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link + "might be down!")
+		c <- link
 		return
 	}
 	fmt.Println(link, "is up!")
+	c <- link
 }
